9469ddc2c9c937ab0c1f151609d7408b45b7e861
黄树鹏 <825844231@qq.com>
2019-03-30 15:41:12 +0800

    Initial Commit

---
+++ SPCommonLibraryTests/SPCommonLibraryTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPCommonLibraryTests.swift
+//  SPCommonLibraryTests
+//
+//  Created by 黄树鹏 on 2019/3/30.
+//  Copyright © 2019 Peng. All rights reserved.
+//
+
+import XCTest
+@testable import SPCommonLibrary
+
+class SPCommonLibraryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
