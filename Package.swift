8bb9a59c8a83929d5f2eeebda39f12bad021616f
黄树鹏 <825844231@qq.com>
2019-07-26 14:50:41 +0800

    移目录

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPCommonLibrary",
-            dependencies: ["Alamofire","SnapKit"]),
+            dependencies: ["Alamofire","SnapKit"],path :"Sources"),
         .testTarget(
             name: "SPCommonLibraryTests",
             dependencies: ["SPCommonLibrary"]),
