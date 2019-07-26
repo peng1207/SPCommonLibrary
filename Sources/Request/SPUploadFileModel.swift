cb5821b62c6346945ea2d630680f2da1b150a7b4
黄树鹏 <825844231@qq.com>
2019-07-26 18:23:31 +0800

    更改文件权限

--- Sources/Request/SPUploadFileModel.swift
+++ Sources/Request/SPUploadFileModel.swift
@@ -7,26 +7,26 @@
 //  上传图片的mode
 
 import Foundation
-class SPUploadFileModel {
+public class SPUploadFileModel {
     /// 上传的路径
-    var url : String?
+    public var url : String?
     /// 参数
-    var parm : [String : Any]?
+    public var parm : [String : Any]?
     /// 请求返回值类型
-    var reponseFormat : SPReponseFormat = .json
+    public var reponseFormat : SPReponseFormat = .json
     /// 文件数组
-    var dataList : [SPUploadFileStruct]?
-    var isRequest : Bool = false
+    public var dataList : [SPUploadFileStruct]?
+    public var isRequest : Bool = false
 }
 
-struct SPUploadFileStruct {
+public struct SPUploadFileStruct {
     /// 上传的文件
-    var data : Data?
+    public var data : Data?
     /// 后台检图片类型的，主要是后缀名
-    var fileName : String?
+    public var fileName : String?
     /// 传输的文件类型
-    var mimeType : String?
+    public var mimeType : String?
     /// 后台通过字段来获取图片
-    var name : String
+    public var name : String
     
 }
